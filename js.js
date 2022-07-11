c5e70ab7143e6de315260194f009b2e43b6ced6e
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-07-11 20:50:32 +0500

    7-8sec footer add

---
+++ js.js
@@ -0,0 +1,28 @@
+// let firsName = "Abduqodir";
+
+// let obj = {
+//     Ism: "Abduqodir",
+//     Familiya: "Urinov",
+//     Yosh: 16,
+//     Maktab: "IZB" 
+// }
+
+// console.log(obj);
+
+// let json = `{
+// 	"a": "aaa",
+// 	"b": "bbb",
+// 	"c": "ccc",
+// 	111: "ddd"
+// }`;
+
+// let json = `[
+// 	{
+// 		"a": "aaa",
+// 		"b": "bbb"
+// 	},
+// 	{
+// 		"c": "ccc",
+// 		"d": "ddd"
+// 	}
+// ]`;
